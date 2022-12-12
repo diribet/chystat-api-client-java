841c64744c9506a8009541d5931d4f487858296c
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2022-12-12 21:17:12 +0100

    increase default client read timeout to 1 minute

--- src/main/java/cz/diribet/chystat/api/client/impl/ChystatApiClient.java
+++ src/main/java/cz/diribet/chystat/api/client/impl/ChystatApiClient.java
@@ -1,14 +1,19 @@
 package cz.diribet.chystat.api.client.impl;
 
+import java.time.Duration;
+
 import com.google.gson.Gson;
 
 import cz.diribet.chystat.api.client.jwt.JwtTokenProvider;
+import okhttp3.OkHttpClient;
 import okhttp3.Request.Builder;
 
 /**
  * {@link OkHttpApiClient} that creates authenticated requests using the given {@link JwtTokenProvider}
  */
 public class ChystatApiClient extends OkHttpApiClient {
+
+	private static final Duration READ_TIMEOUT = Duration.ofMinutes(1);
 
 	private final JwtTokenProvider jwtTokenProvider;
 
@@ -19,6 +24,13 @@ public class ChystatApiClient extends OkHttpApiClient {
 	public ChystatApiClient(String baseUrl, Gson gson, JwtTokenProvider jwtTokenProvider) {
 		super(baseUrl, gson);
 		this.jwtTokenProvider = jwtTokenProvider;
+	}
+
+	@Override
+	protected OkHttpClient.Builder clientBuilder() {
+		OkHttpClient.Builder builder = super.clientBuilder();
+		builder.readTimeout(READ_TIMEOUT);
+		return builder;
 	}
 
 	@Override
